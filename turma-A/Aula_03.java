3b0d64d071e7894d5bcf1234038ba2bace781a2e
maysemog <133466664+maysemog@users.noreply.github.com>
2023-07-08 14:26:36 +0000

    matrizes

--- turma-A/Aula_03.java
+++ turma-A/Aula_03.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Aula_03 {
     public static void main(String[] args) {
@@ -91,10 +92,143 @@ public class Aula_03 {
         System.out.println("A média dos valores é: "+media);
         */
 
+        /*
+        for (int i=1; i<=1000; i++){
+        
+            if (i % 10 == 0){
+                System.out.println(i);
+
+            }
+            
+        }
+        */
+        //int numeros[] = {1,5,6,2,9};
+        /* int numeros[] = new int[5];
+        numeros[0]= 1;
+        numeros[1]= 5;
+        numeros[2]= 6;
+        numeros[3]= 2;
+        numeros[4]= 9;
+         */
+
+         //int numeros[] = {1,5,6,2,9};
+         //int soma= numeros[2]+numeros[3];
+         //System.out.println(soma);
+         //imprime: 8
+
+         /*
+          * String cidades[] = {"São Paulo", "Rio de Janeiro"};
+          char caracteres[] = new char[256];
+          */
+
+            //char alfabeto[] = {'a'};
+            //int tamanho = alfabeto.lenght();
+            //System.out.println(tamanho);
+            //imprime 1
+
+        /*
+        int numero[] = {2,4,14};
+        int soma = numero[0]+numero[1]+numero[2];
+        System.out.println(soma);
+        */
+
+        /*
+        String nomes[] = {"São Paulo", "Londres", "J"};
+        for (String n:nomes){
+            //System.out.println(n+"");
+            System.out.print(n+"");
+        }
+
+        String nome[] = {"São Paulo", "Londres", "J", "Nerdola"};
+        System.out.println(nomes.length()+""+nome[0]);
+        */
+
+
+        /*
+        //dando entrada de valores com arrays
+        int val[] = new int[5];
+
+        for(int i = 0; i < val.length; i++) {
+            val[i] = scn.nextInt();
+        }
+        */
+
+        /*
+        System.out.print("Digite o tamanho do vetor: ");
+        int tamanho = scn.nextInt();
+        int mult = 1;
+        double media = 0;
+
+        int val[] = new int[tamanho];
+
+        for (int i = 0; i < val.length; i++) {
+            System.out.print("Digite o "+(i+1)+"º número: ");
+            val[i] = scn.nextInt();
+            mult *= val[i];
+            media += val[i];
+        }
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            System.out.print("Os valores são: ");
+        for (int j = 0; j < val.length;j++){
+            System.out.print(val[j]+" ");
+        }
+        System.out.println("");
+        System.out.println("A multiplicação dos valores são: "+mult);
+        media /= val.length;
+        System.out.println("A média dos valores é: "+media);
+        */
+
+        /*
+        System.out.print("Digite a quantidade de valores no array: ");
+        int quant = scn.nextInt();
+
+        int arr[] = new int[quant];
+       
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Digite o valor do indice "+i+": ");
+            arr[i] = scn.nextInt();
+            
+        }
+        Arrays.sort(arr);
+        System.out.print("O valor "+arr[0]+" é o menor número, já o maior é o número "+ arr[quant-1]);
+        */
+        
+        //int [] [] tabela = {{0,1},{2,3}};
+        //int lin = 2;
+        /*
+        int tabela[][] = new int [lin][2];
+        tabela[0][0] = 0;
+        tabela[0][1] = 1;
+        tabela[1][0] = 2;
+        tabela[1][1] = 3;
+        */
+        /*
+        int soma = tabela[0][1] + tabela[1][1];
+        System.out.println(soma);
+
+        for(int i=0; i< lin; i++){
+            for(int j=0; j<col; j++) {
+                System.out.print(tabela[i][j] + " ");
+            } 
+            System.out.println("");
+        }
+        */
+
+        /*
+        int[] [] tabela = {{4,6},{1,7}};
+
+        for (int i = 0; i < 2; i++) { for (int j = 0; j < 2; j++){ System.out.print(tabela[i][j]+" ");}System.out.println("");} 
+        */
+
+
+        int lin = 3;
+        int col = 3;
+        int tabela[][] = new int [lin][col];
 
         
 
 
-
-    }
+    }   
 }
+
+        
